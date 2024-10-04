9c99201c060a0db3535806444a0c6332fe4c443b
ChristianP-code <christianpoed@gmail.com>
2024-10-04 08:14:51 +0100

    Add playRound function
    playRound function takes humanChoice and computerChoice
    and logs a message and increments a score depending on a
    win or lose for either player

--- script.js
+++ script.js
@@ -5,22 +5,19 @@ let getRandomNbr = Math.floor(Math.random() * 3);
 // Returns a random number between 0-2 inclusive
 switch(getRandomNbr) {
     case 0:
-        return "Rock";
+        return "rock";
     case 1:
-        return "Paper";
+        return "paper";
     case 2:
-        return "Scissors"
+        return "scissors"
     }
-//console.log(getRandomNbr);
 }
 
 function getHumanChoice() {
-    let getInput = window.prompt("Make your choice! Type rock, paper, or scissors:");
-    // check that user input matches one of the choices: rock, paper, scissors
-    
+    let getInput = window.prompt("Make your choice! Type rock, paper, or scissors:"); 
         switch (getInput.toLowerCase()) {
             case "rock":
-                alert("You\'ve chosen rock!");
+                alert("You\'ve chosen rock!"); //need to change alerts to console.log
                 return "rock";
             case "paper":
                 alert("You\'ve chosen paper!");
@@ -33,5 +30,38 @@ function getHumanChoice() {
             getHumanChoice();
         }
 }
-
 getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+// RPS game logic:   
+// if human and computer choices are the same, it's a draw and no score increment
+// human rock vs comp scissors = win, so increment human score
+// human paper vs rock = win, so increment human score
+// human scissors vs paper = win, so increment human score
+// anything else is a human loss, so increment computer score
+
+    if (humanSelection === computerSelection) {
+        console.log(`It\'s a draw! You both chose ${humanSelection}!`);
+    } else if (humanSelection === "rock" && computerSelection === "scissors") {
+        console.log(`You win! ${humanSelection} beats ${computerSelection}!`);
+        humanScore++;
+    } else if (humanSelection === "paper" && computerSelection === "rock"){
+        console.log(`You win! ${humanSelection} beats ${computerSelection}!`);
+        humanScore++;
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
+        console.log(`You win! ${humanSelection} beats ${computerSelection}!`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${humanSelection}!`)
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice;
+const computerSelection = getComputerChoice;
+
+playRound(humanSelection, computerSelection);
